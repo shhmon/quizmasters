54b85999c5e05ba6c43b9056171e6eafd9452d57
Johannes Bastmark <bastmark@outlook.com>
2020-02-24 13:17:15 +0100

    Small changes for header and added header. No logic implemented

--- quiz-app/src/app/app.component.ts
+++ quiz-app/src/app/app.component.ts
@@ -1,13 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-
 export class AppComponent {
-  title = 'quiz-app';
+  title = "quiz-app";
 
-  constructor(){}
+  constructor() {}
 }
--- quiz-app/src/app/app.module.ts
+++ quiz-app/src/app/app.module.ts
@@ -1,21 +1,34 @@
+// Angular
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from "@angular/common/http";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AngularSvgIconModule } from "angular-svg-icon";
 
+// Material
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatIconModule } from "@angular/material/icon";
+
+// App
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import {MatButtonModule} from '@angular/material/button';
-import { WordsComponent } from './words/words.component';
+import { WordsComponent } from "./words/words.component";
+import { HeaderComponent } from "./header/header.component";
 
 @NgModule({
-  declarations: [AppComponent, WordsComponent],
+  declarations: [AppComponent, WordsComponent, HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    AngularSvgIconModule.forRoot(),
+    MatMenuModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
