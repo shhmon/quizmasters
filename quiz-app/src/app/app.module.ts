7272919cc9851ee78d9773cb5a5567cc9bfc1255
Johannes Bastmark <bastmark@outlook.com>
2020-02-25 10:17:22 +0100

    added routing and template component for highscores

--- quiz-app/src/app/app.module.ts
+++ quiz-app/src/app/app.module.ts
@@ -4,24 +4,36 @@ import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AngularSvgIconModule } from "angular-svg-icon";
+import { RouterModule, Routes } from "@angular/router";
 
 // Material
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatButtonModule } from "@angular/material/button";
 import { MatMenuModule } from "@angular/material/menu";
 import { MatIconModule } from "@angular/material/icon";
-import { MatGridListModule } from '@angular/material/grid-list';
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatCardModule } from "@angular/material/card";
 
 // App
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { WordsComponent } from "./words/words.component";
 import { HeaderComponent } from "./header/header.component";
-import { MatCardModule } from "@angular/material/card";
-import { FooterComponent } from './footer/footer.component';
+import { FooterComponent } from "./footer/footer.component";
+import { HighscoresComponent } from "./highscores/highscores.component";
 
+const appRoutes: Routes = [
+  { path: "", component: WordsComponent },
+  { path: "highscores", component: HighscoresComponent }
+];
 @NgModule({
-  declarations: [AppComponent, WordsComponent, HeaderComponent, FooterComponent],
+  declarations: [
+    AppComponent,
+    WordsComponent,
+    HeaderComponent,
+    FooterComponent,
+    HighscoresComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -32,8 +44,12 @@ import { FooterComponent } from './footer/footer.component';
     AngularSvgIconModule.forRoot(),
     MatMenuModule,
     MatIconModule,
-		MatCardModule,
-		MatGridListModule
+    MatCardModule,
+    MatGridListModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
