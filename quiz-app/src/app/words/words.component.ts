ba10f45c24144a24908b0eabe175874d532f8ee6
Johannes Bastmark <bastmark@outlook.com>
2020-02-25 09:50:08 +0100

    color fix

--- quiz-app/src/app/words/words.component.ts
+++ quiz-app/src/app/words/words.component.ts
@@ -1,31 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpService } from '../http.service';
+import { Component, OnInit } from "@angular/core";
+import { HttpService } from "../http.service";
 
 @Component({
-  selector: 'app-words',
-  templateUrl: './words.component.html',
-  styleUrls: ['./words.component.css']
+  selector: "app-words",
+  templateUrl: "./words.component.html",
+  styleUrls: ["./words.component.css"]
 })
 export class WordsComponent implements OnInit {
-
-  constructor(private http: HttpService){
+  constructor(private http: HttpService) {
     this.handleClick = this.handleClick.bind(this);
-	}
+  }
 
-	question = {words:[], definition:String, answer:String, score:Number}
-	guess : String;
+  question = { words: [], definition: String, answer: String, score: Number };
+  guess: String;
   round = 1;
 
   ngOnInit(): void {
-      this.http.fetchData().subscribe(data => {
-				this.question = JSON.parse(JSON.stringify(data));
-      })
-    }
+    this.http.fetchData().subscribe(data => {
+      this.question = JSON.parse(JSON.stringify(data));
+    });
+    this.guess = "";
+  }
 
-  handleClick(event){
-		this.guess = event.currentTarget.value;
-    this.round+=1;
+  handleClick(event) {
+    this.guess = event.currentTarget.value;
+    this.round += 1;
     setTimeout(() => {
-    this.ngOnInit()}, 1000);
+      this.ngOnInit();
+    }, 1000);
   }
 }
