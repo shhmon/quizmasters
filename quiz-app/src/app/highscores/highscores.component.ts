4c0519bea531901fcefdc0df442655f0959bf190
Johannes Bastmark <bastmark@outlook.com>
2020-02-26 14:08:38 +0100

    template bug fix where application didnt start

--- quiz-app/src/app/highscores/highscores.component.ts
+++ quiz-app/src/app/highscores/highscores.component.ts
@@ -1,22 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { DataServiceService } from "../data-service.service"
+import { Component, OnInit } from "@angular/core";
+import { DataServiceService } from "../data-service.service";
 
 @Component({
-  selector: 'app-highscores',
-  templateUrl: './highscores.component.html',
-  styleUrls: ['./highscores.component.css'],
-  template: `<app-words>(messageEvent)="receiveMessage($event)" </app-words>`
+  selector: "app-highscores",
+  templateUrl: "./highscores.component.html",
+  styleUrls: ["./highscores.component.css"]
 })
 export class HighscoresComponent implements OnInit {
-
-  constructor(private data: DataServiceService) { }
-
+  constructor(private data: DataServiceService) {}
 
   ngOnInit(): void {
     this.data.currentMessage.subscribe(message => {
-      window.localStorage.setItem('scores', JSON.stringify(message))
+      window.localStorage.setItem("scores", JSON.stringify(message));
     });
-    console.log(window.localStorage.getItem('scores'));
+    console.log(window.localStorage.getItem("scores"));
   }
-
 }
